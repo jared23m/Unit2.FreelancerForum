5b4cb4456f2259d99d627bb20a2f883bfe3193c6
Jared May <jared23m@gmail.com>
2023-12-02 22:45:44 -0600

    Steps left: implement timer, edit to fit rubric

--- FreelancerForum.js
+++ FreelancerForum.js
@@ -1,10 +1,11 @@
+// sets array of current freelancers
 let currentFLs = [
     { name: "Dr. Slice", price: 25, occupation: "gardener" },
     { name: "Dr. Pressure", price: 51, occupation: "programmer" },
     { name: "Prof. Possibility", price: 43, occupation: "teacher" },
     ];
 
-
+// sets array of the freelancer backlog
 let backLogFLs = [ 
     { name: "Prof. Prism", price: 81, occupation: "teacher" },
     { name: "Dr. Impulse", price: 43, occupation: "teacher" },
@@ -13,51 +14,71 @@ let backLogFLs = [
     { name: "Prof. Goose", price: 72, occupation: "driver" },
   ];
 
-let newPriceSum = 0;
-let newPriceAvg = 0;
+// creates important variables for later
+let newPriceSum;
+let newPriceAvg;
+let avgPrint;
+let entryPrint;
+let addPrint;
+let amountOfNew = 0;
+let body;
 
+// this function prints initial freelancers and average price at top
 function initialPrint(initialInfo) {
 
+    // calculates initial average price
     newPriceSum = initialInfo.reduce((accumulator, currentValue) => {
         return accumulator + currentValue.price;
     }, 0);
+    newPriceAvg = Math.round(newPriceSum/(initialInfo.length));
 
-    newPriceAvg = newPriceSum/(initialInfo.length);
-
-    const body = document.querySelector("body");
-    let avgPrint;
-    let entryPrint;
-
+    // prints initial average price
+    body = document.querySelector("body");
     avgPrint = document.createElement("h1");
     avgPrint.setAttribute("id", "average");
     avgPrint.textContent = "Average Price: " + newPriceAvg;
     body.append(avgPrint);
 
+    // prints initial freelancers in string form
     initialInfo.forEach((entry) => {
         entryPrint = document.createElement("h1");
-        entryPrint.textContent = "Name: " + entry.name + "  Price: " + entry.price + "  Occupation: " + entry.occupation;
+        entryPrint.textContent = "Name: " + entry.name + "_____Price: " + entry.price + "_____Occupation: " + entry.occupation;
         body.append(entryPrint);
     });
   
 }
 
+/* this function grabs a random freelancer from the backlog and prints
+it out, while also updating the average price */
 function generateAndPrint(initial, bank) {
+
+    /* grabs random freelancer from backlog and removes it from backlog
+    after it is stored in a variable */
     const randIndex = Math.round(Math.random() * (bank.length - 1));
     const newFL = bank[randIndex];
     bank.splice(randIndex, 1);
 
+    /* remembers how many new freelancers have been added so it can
+    increase the divisor when calculating the average */
+    amountOfNew++;
+
+    // calculates new average price
     newPriceSum = newPriceSum + newFL.price;
+    newPriceAvg = Math.round(newPriceSum/(initial.length + amountOfNew));
 
-    newPriceAvg = newPriceSum/(initial.length);
+    // updates average price on screen
+    avgPrint.textContent = "Average Price: " + newPriceAvg;
 
-    // still need to update HTML so that average shows up
-
-    const body = document.querySelector("body");
-    let addPrint = document.createElement("h1");
-    addPrint.textContent = "Name: " + newFL.name + "  Price: " + newFL.price + "  Occupation: " + newFL.occupation;
+    // prints new freelancer below list in string form
+    body = document.querySelector("body");
+    addPrint = document.createElement("h1");
+    addPrint.textContent = "Name: " + newFL.name + "_____Price: " + newFL.price + "_____Occupation: " + newFL.occupation;
     body.append(addPrint);
 }
 
 
 // still need final code where functions are called
+
+initialPrint(currentFLs);
+generateAndPrint(currentFLs, backLogFLs);
 
